92d02abfb9002ed0c956b01d9a6e8f902a501e55
lambac <lamdum@yahoo.com>
2021-12-19 16:57:24 +0100

    move also haproxy cfg settings

--- backup_docker.sh
+++ backup_docker.sh
@@ -19,13 +19,14 @@ docker ps |awk 'BEGIN {first = 1; last = 2 }
 
 echo "echo \">>>>>>>>>>>>>> ARCHAVING IOTStack folder................\n\"" >> execute_backup.sh
 echo "sudo tar -czvf IOTstack.tar.gz /home/pi/IOTstack/" >>  execute_backup.sh
-
+echo "cp /etc/haproxy/haproxy.cfg /home/pi/containers/" >> execute_backup.sh
 
 if [ -d "$DIR_EX_DRIVE" ]; then
   ### Take action if $DIR exists ###
   echo "echo \"Copy containers files in ${DIR}...\"" >> execute_backup.sh
   echo "sudo cp -r  /home/pi/containers/* ${DIR_EX_DRIVE} " >> execute_backup.sh
-  echo "sudo cp -r  `pwd`/IOTstack.tar.gz ${DIR_EX_DRIVE} " >> execute_backup.sh  
+  echo "sudo cp -r  `pwd`/IOTstack.tar.gz ${DIR_EX_DRIVE} " >> execute_backup.sh
+  echo "cp /etc/haproxy/haproxy.cfg ${DIR_EX_DRIVE} " >> execute_backup.sh 
 else
   ###  Control will jump here if $DIR does NOT exists ###
   echo "echo \"Error: ${DIR_EX_DRIVE} not found. Can not continue.\"" >> execute_backup.sh
