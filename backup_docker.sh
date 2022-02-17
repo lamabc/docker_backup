d834148088a2dc3e9e304c29fc166eba69b418cf
lambac <lamdum@yahoo.com>
2022-02-17 18:24:20 +0100

    do some bug fixing for copying only the files which are created

--- backup_docker.sh
+++ backup_docker.sh
@@ -7,9 +7,9 @@ FILENAME="execute_backup.sh"
 DATE=$(echo $DATE |sed -r 's/[ :]+/_/g')
 ACTUALSIZE=$(df --output=target,avail|awk '{print $1 $2}'|grep -m 1 "^/[0-9]*$"|sed "s/\///")
 ACTUAL_SIZE_BACKUP_DRIVE=$(df --output=target,avail|awk '{print $1 $2}'|grep -m 1 "^$MOUNT_POINT_BACKUP_DRIVE[0-9]*$"|sed 's,'"$MOUNT_POINT_BACKUP_DRIVE"',,')
-NOT_LESS_THEN_SIZE_TO_PERFORM_DELETE=410163640
+NOT_LESS_THEN_SIZE_TO_PERFORM_DELETE=110163640
 SIZE_OF_DISK_TO_SKIP_BACKUP=5163640
-NUMBER_OF_DAYS=5
+NUMBER_OF_DAYS=30
 
 echo "" > execute_backup.sh
 
@@ -50,19 +50,20 @@ sed -i 's,'"XXX"','"$CONT_FOLDER"',' $FILENAME
 sed -i "s/.tar.gz/_${DATE}.tar.gz/" $FILENAME
 echo "echo \">>>>>>>>>>>>>> ARCHAVING IOTStack folder................\n\"" >> execute_backup.sh
 echo "sudo tar -czvf IOTstack_${DATE}.tar.gz /home/pi/IOTstack/" >>  execute_backup.sh
+echo "echo \"MOVING IOTStack folder backup to containers folder...\n\"" >> execute_backup.sh
 echo "mv IOTstack_${DATE}.tar.gz ${CONT_FOLDER} " >> execute_backup.sh
+echo "echo \"Copying haproxy file to backup...\n\"" >> execute_backup.sh
 echo "cp /etc/haproxy/haproxy.cfg ${CONT_FOLDER}haproxy_${DATE}.cfg" >> execute_backup.sh
 
 
 if [ -d "$DIR_EX_DRIVE" ]; then
   ### Take action if $DIR exists ###
   echo "echo \"Copy containers files in ${DIR_EX_DRIVE}...\"" >> execute_backup.sh
-  echo "sudo cp -r  ${CONT_FOLDER}* ${DIR_EX_DRIVE} " >> execute_backup.sh
-#  echo "sudo cp -r  ${CONT_FOLDER}IOTstack_${DATE}.tar.gz ${DIR_EX_DRIVE} " >> execute_backup.sh
-  echo "cp ${CONT_FOLDER}haproxy_${DATE}.cfg ${DIR_EX_DRIVE} " >> execute_backup.sh 
+  echo "cd ${CONT_FOLDER}"  >> execute_backup.sh
+  echo "sudo ls -haltr|egrep ${DATE}.*|awk {'print \$9'}|xargs cp -t ${DIR_EX_DRIVE} " >> execute_backup.sh 
 else
   ###  Control will jump here if $DIR does NOT exists ###
   echo "echo \"Error: ${DIR_EX_DRIVE} not found. Can not continue.\"" >> execute_backup.sh
 fi
 
-#. execute_backup.sh
+. execute_backup.sh
--- execute_backup.sh
+++ execute_backup.sh
@@ -1,24 +1,22 @@
 
-echo "Deleting old files to free disk /dev/root/..."
-sudo find /home/pi/containers/ -type f -mtime +5 -delete
-echo "Deleting old files to free disk /dev/sdb1..."
-sudo find /media/pi/DriveCos/IOT_boot_camp/save/ -type f -mtime +5 -delete
 echo iotstack_zigbee2mqtt_latest tar is creating on local drive...
-docker export 2ecad1f54ee7 |gzip > /home/pi/containers/iotstack_zigbee2mqtt_latest_Thu_17_Feb_2022_04_34_43_PM_CET.tar.gz
+docker export 2ecad1f54ee7 |gzip > /home/pi/containers/iotstack_zigbee2mqtt_latest_Thu_17_Feb_2022_05_44_53_PM_CET.tar.gz
 echo iotstack_mosquitto_latest tar is creating on local drive...
-docker export b3807defaeeb |gzip > /home/pi/containers/iotstack_mosquitto_latest_Thu_17_Feb_2022_04_34_43_PM_CET.tar.gz
+docker export b3807defaeeb |gzip > /home/pi/containers/iotstack_mosquitto_latest_Thu_17_Feb_2022_05_44_53_PM_CET.tar.gz
 echo grafana__grafana tar is creating on local drive...
-docker export 58691a824bfd |gzip > /home/pi/containers/grafana__grafana_Thu_17_Feb_2022_04_34_43_PM_CET.tar.gz
+docker export 58691a824bfd |gzip > /home/pi/containers/grafana__grafana_Thu_17_Feb_2022_05_44_53_PM_CET.tar.gz
 echo portainer__portainer-ce tar is creating on local drive...
-docker export a172110877d4 |gzip > /home/pi/containers/portainer__portainer-ce_Thu_17_Feb_2022_04_34_43_PM_CET.tar.gz
+docker export a172110877d4 |gzip > /home/pi/containers/portainer__portainer-ce_Thu_17_Feb_2022_05_44_53_PM_CET.tar.gz
 echo iotstack_nodered tar is creating on local drive...
-docker export bbcf21a4032f |gzip > /home/pi/containers/iotstack_nodered_Thu_17_Feb_2022_04_34_43_PM_CET.tar.gz
+docker export bbcf21a4032f |gzip > /home/pi/containers/iotstack_nodered_Thu_17_Feb_2022_05_44_53_PM_CET.tar.gz
 echo influxdb_1.8 tar is creating on local drive...
-docker export 719cda2a5f3e |gzip > /home/pi/containers/influxdb_1.8_Thu_17_Feb_2022_04_34_43_PM_CET.tar.gz
+docker export 719cda2a5f3e |gzip > /home/pi/containers/influxdb_1.8_Thu_17_Feb_2022_05_44_53_PM_CET.tar.gz
 echo ">>>>>>>>>>>>>> ARCHAVING IOTStack folder................\n"
-sudo tar -czvf IOTstack_Thu_17_Feb_2022_04_34_43_PM_CET.tar.gz /home/pi/IOTstack/
-mv IOTstack_Thu_17_Feb_2022_04_34_43_PM_CET.tar.gz /home/pi/containers/ 
-cp /etc/haproxy/haproxy.cfg /home/pi/containers/haproxy_Thu_17_Feb_2022_04_34_43_PM_CET.cfg
+sudo tar -czvf IOTstack_Thu_17_Feb_2022_05_44_53_PM_CET.tar.gz /home/pi/IOTstack/
+echo "MOVING IOTStack folder backup to containers folder...\n"
+mv IOTstack_Thu_17_Feb_2022_05_44_53_PM_CET.tar.gz /home/pi/containers/ 
+echo "Copying haproxy file to backup...\n"
+cp /etc/haproxy/haproxy.cfg /home/pi/containers/haproxy_Thu_17_Feb_2022_05_44_53_PM_CET.cfg
 echo "Copy containers files in /media/pi/DriveCos/IOT_boot_camp/save/..."
-sudo cp -r  /home/pi/containers/* /media/pi/DriveCos/IOT_boot_camp/save/ 
-cp /home/pi/containers/haproxy_Thu_17_Feb_2022_04_34_43_PM_CET.cfg /media/pi/DriveCos/IOT_boot_camp/save/ 
+cd /home/pi/containers/
+sudo ls -haltr|egrep Thu_17_Feb_2022_05_44_53_PM_CET.*|awk {'print $9'}|xargs cp -t /media/pi/DriveCos/IOT_boot_camp/save/ 
